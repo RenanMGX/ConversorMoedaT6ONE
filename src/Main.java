d64bfae5a37ab8c836ba9c44bbc35e38f264d5fe
renanmgx <renanmgx@hotmail.com>
2024-05-02 23:20:09 -0300

    adicionando entradas do usuario

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import API.ApiExchange;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -12,6 +15,58 @@ public class Main {
         Double valor = conversor.ConvertTo("USD", "BRL", 4500);
 
 
+
+        Map<String, List<String>> parameters = new HashMap<>();
+        parameters.put("sair", List.of(
+                "sair", "exit", "/s", "bye"
+        ));
+        parameters.put("help", List.of(
+                "help", "/h"
+        ));
+        parameters.put("converter", List.of(
+                "converter", "iniciar", "start", "/s"
+        ));
+
+        //Scanner scanner = new Scanner(System.in);
+        //String input = scanner.nextLine();
+        //System.out.println(input.toLowerCase());
+        //System.out.println(input.toLowerCase().equals("sair"));
+
+
+        System.out.println("Iniciando Programa ConversorMoedaT6ONE");
+        System.out.println("no caso de duvidas escreva 'help' para ajuda!");
+        while (true){
+            System.out.printf("Digite um comando: ");
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+
+            if (parameters.get("sair").contains(input.toLowerCase())){
+                break;
+            } else if (parameters.get("help").contains(input.toLowerCase())) {
+                for (Map.Entry<String, List<String>> entry : parameters.entrySet()){
+                    if (entry.getKey().equals("sair")) {
+                        System.out.println("\nParametros para encerrar o programa:");
+                        System.out.println(entry.getValue());
+                        System.out.println();
+
+
+                        } else if (entry.getKey().equals("converter")) {
+                        System.out.println("\nParametros para iniciar a conversão:");
+                        System.out.println(entry.getValue());
+                        System.out.println();
+                    }else {
+                        System.out.println(entry.getValue());
+                    }
+                }
+            } else if (parameters.get("converter").contains(input.toLowerCase())) {
+
+            }
+
+
+            System.out.println();
+
+
+        }
 
     }
 }
